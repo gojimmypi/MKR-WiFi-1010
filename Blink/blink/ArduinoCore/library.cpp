476855895ce40778d028361cfbf016aef8baaf77
gojimmypi <gojimmypi@gmail.com>
2018-12-09 12:22:16 -0800

    initial code

---
+++ Blink/blink/ArduinoCore/library.cpp
@@ -0,0 +1,15 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 12/8/2018 6:13:45 PM
+ * Author : gojimmypi
+ */ 
+
+#include "sam.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
